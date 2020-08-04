d0902f3cc9f3dadbd9db69700dad326a5cf54095
RIchard Herald <ric.herald@gmail.com>
2020-08-04 12:04:58 -0300

    Install ex_machina

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule BankApi.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:bcrypt_elixir, "~> 2.0"}
+      {:bcrypt_elixir, "~> 2.0"},
+      {:ex_machina, "~> 2.4", only: :test}
     ]
   end
 
