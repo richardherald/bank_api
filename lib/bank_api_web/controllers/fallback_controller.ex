cb15a3c2fdd9541b7b9c65ce98173e87d6058367
RIchard Herald <ric.herald@gmail.com>
2020-08-13 13:03:58 -0300

    Add Transaction API

--- lib/bank_api_web/controllers/fallback_controller.ex
+++ lib/bank_api_web/controllers/fallback_controller.ex
@@ -47,6 +47,16 @@ defmodule BankApiWeb.FallbackController do
     |> render("error_message.json", message: "Account not found")
   end
 
+  def call(conn, {:error, message}) when message == :invalid_date_format do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(BankApiWeb.ErrorView)
+    |> render("error_message.json",
+      message:
+        "Invalid date format. Check that parameters start_date or end_date are in the format yyyy-MM-dd"
+    )
+  end
+
   def call(conn, {:error, message}) do
     conn
     |> put_status(401)
---
+++ lib/bank_api_web/controllers/transaction_controller.ex
@@ -0,0 +1,16 @@
+defmodule BankApiWeb.TransactionController do
+  use BankApiWeb, :controller
+
+  alias BankApi.Transactions.Transactions
+  alias BankApiWeb.Guardian
+
+  action_fallback BankApiWeb.FallbackController
+
+  def transactions(conn, %{"start_date" => start_date, "end_date" => end_date}) do
+    user = Guardian.Plug.current_resource(conn)
+
+    with {:ok, transactions} <- Transactions.run(user.accounts.id, start_date, end_date) do
+      render(conn, "show.json", transactions: transactions)
+    end
+  end
+end
