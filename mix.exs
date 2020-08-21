4327ff95417ef234cdc59f53afb3836ea638b0ec
RIchard Herald <ric.herald@gmail.com>
2020-08-21 07:05:21 -0300

    Replacing encryption library

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule BankApi.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:bcrypt_elixir, "~> 2.0"},
+      {:argon2_elixir, "~> 2.0"},
       {:ex_machina, "~> 2.4", only: :test},
       {:guardian, "~> 2.0"},
       {:distillery, "~> 2.1", runtime: false},
